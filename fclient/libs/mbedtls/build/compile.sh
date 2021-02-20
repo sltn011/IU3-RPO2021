70b2166948eaf693c5a81b3ade79c585de72b0e7
sltn011 <tenyaev011@gmail.com>
2021-02-21 02:47:58 +0300

    fclient first commit
    spdlog + mbedcrypto linked successfully to x86_64

---
+++ fclient/libs/mbedtls/build/compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#ABI=armaebi-v7a
+#ABI=x86
+#ABI=arm64-v8a
+ABI=x86_64
+
+ANDROID_NDK="D:/VedroidSDK/ndk/21.1.6352462"
+TOOL_CHAIN="${ANDROID_NDK}/build/cmake/android.toolchain.cmake"
+CMAKE="D:/VedroidSDK/cmake/3.10.2.4988404/bin/cmake.exe"
+
+mkdir -p ${ABI}
+cd ${ABI}
+
+${CMAKE} -G Ninja ../../mbedtls -DCMAKE_SYSTEM_NAME=Android -DCMAKE_SYSTEM_VERSION=21 \
+-DANDROID_ABI=${ABI} -DCMAKE_TOOLCHAIN_FILE=${TOOL_CHAIN} -DCMAKE_MAKE_PROGRAM=ninja.exe \
+-DUSE_SHARED_MBEDTLS_LIBRARY=On -DENABLE_TESTING=Off
+
+${CMAKE} --build .
